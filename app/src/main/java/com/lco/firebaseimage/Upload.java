f081e7bd8ccf61166a35490f6aa56038a2fe4500
asifkhanonline <edu.asifkhan11@gmail.com>
2019-07-14 01:04:29 +0530

    first commit

---
+++ app/src/main/java/com/lco/firebaseimage/Upload.java
@@ -0,0 +1,32 @@
+package com.lco.firebaseimage;
+
+public class Upload {
+    public String name;
+    public String url;
+
+    // Default constructor required for calls to
+    // DataSnapshot.getValue(User.class)
+    public Upload() {
+    }
+
+    public Upload(String name, String url) {
+        this.name = name;
+        this.url= url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
